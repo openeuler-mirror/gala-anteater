0883a8dd11d68ec806605e5657704a54ce6db114
lizhenxing11 <lizhenxing11@huawei.com>
2022-11-10 15:30:50 +0800

    Update the Copyright Document for All Python File

--- anteater/model/smoother.py
+++ anteater/model/smoother.py
@@ -1,3 +1,16 @@
+#!/usr/bin/python3
+# ******************************************************************************
+# Copyright (c) 2023 Huawei Technologies Co., Ltd.
+# gala-anteater is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+# ******************************************************************************/
+
 import numpy as np
 from scipy.signal import savgol_filter
 
--- anteater/template/app_anomaly_template.py
+++ anteater/template/app_anomaly_template.py
@@ -1,4 +1,15 @@
-import re
+#!/usr/bin/python3
+# ******************************************************************************
+# Copyright (c) 2023 Huawei Technologies Co., Ltd.
+# gala-anteater is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+# ******************************************************************************/
 
 from anteater.template.template import Template
 
@@ -13,21 +24,21 @@ class AppAnomalyTemplate(Template):
 
         result = {
             "Timestamp": timestamp,
-            "event_id": f"{timestamp}_{self.entity_id}",
             "Attributes": {
                 "entity_id": self.entity_id,
                 "event_id": f"{timestamp}_{self.entity_id}",
                 "event_type": "app"
             },
             "Resource": {
-                "metric_id": self.metric_id,
-                "labels": self.labels,
-                "cause_metrics": self.cause_metrics,
+                "metrics": self.metric_id,
+                "metric_label": self.labels,
+                "recommend_metrics": self.cause_metrics,
                 "description": self.description
             },
             "SeverityText": "WARN",
             "SeverityNumber": 13,
-            "Body": f"{self.timestamp.strftime('%c')} WARN, APP may be impacting sli performance issues."
+            "Body": f"{self.timestamp.strftime('%c')} WARN, APP may be impacting sli performance issues.",
+            "event_id": f"{timestamp}_{self.entity_id}",
         }
 
         return result
