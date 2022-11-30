81b051fe5cdff843dad2890a10e8ee9a9735b76b
lizhenxing11 <lizhenxing11@huawei.com>
2022-11-30 09:34:22 +0800

    logging debug info for machine ids and metrics

--- anteater/source/metric_loader.py
+++ anteater/source/metric_loader.py
@@ -97,9 +97,12 @@ class MetricLoader:
         """Gets the unique machine ids based on the metrics"""
         machine_ids = self.get_unique_label(start, end, metrics, label_name="machine_id")
         if not machine_ids:
-            logger.warning(f"Empty unique machine ids!")
+            logger.warning(f'Empty unique machine ids on given metrics!')
+            logger.debug(f'metrics: {";".join(metrics)}')
         else:
-            logger.info(f"Got {len(machine_ids)} unique machine_ids")
+            logger.info(f'Got {len(machine_ids)} unique machine_ids on given metrics')
+            logger.debug(f'machine ids: {";".join(machine_ids)}')
+            logger.debug(f'metrics: {";".join(metrics)}')
         return machine_ids
 
     def get_unique_label(self, start: datetime, end: datetime, metrics: List[str], label_name: str) -> List[str]:
